59fdcba4f8f131a3632e40b5b0950290bfe15203
QueAnhHa <aqh9586@rit.edu>
2019-02-09 11:25:07 -0800

    Refactoring code by moving Sms sending logic to chalicelib.sms module

--- twilioSmsSender/app.py
+++ twilioSmsSender/app.py
@@ -1,14 +1,11 @@
 
-# 3rd party imports
+# Core Imports
 from chalice import Chalice, Response
-from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 
-# Twilio Config
-ACCOUNT_SID = env.get('AC33281374da04c1ea8ca0bde192f5e23c')
-AUTH_TOKEN = env.get('')
-FROM_NUMBER = env.get('FROM_NUMBER') 
-TO_NUMBER =  env.get('TO_NUMBER')
+# App Level imports
+from chalicelib import sms
+
 
 app = Chalice(app_name='twilioSmsSender')
 
@@ -25,16 +22,13 @@ def send_sms():
 	request_body = app.current_request.json_body
 	if request_body:
 		try:
-			msg = tw_client.messages.create(
-				from_=FROM_NUMBER.
-				body=request_body['msg'],
-				to=TO_NUMBER)
-			if msg.sid:
+			resp = sms.send(request_body)
+			if resp:
 				return Response(status_code=201,
-					headers={'Content-Type': 'application/json'},
-					body={'status':'success',
-							'data': msg.sid,
-							'message': 'SMS successfully sent'})
+								headers={'Content-Type': 'application/json'},
+								body={'status':'success',
+									  'data': msg.sid,
+									  'message': 'SMS successfully sent'})
 			else:
 				return Response(status_code=200,
 								headers={'Content-Type': 'application/json'},
